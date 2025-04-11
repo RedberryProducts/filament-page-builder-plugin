1a49532892594ca6040245d960c34b942e487925
GigaGiorgadze <giga.giorgadze.11@gmail.com>
2025-04-11 16:58:24 +0400

    fix: wrong name for arch test

--- tests/ArchTest.php
+++ tests/ArchTest.php
@@ -1,10 +1,10 @@
 <?php
 
-arch('will not use debugging functions')
+it('will not use debugging functions')
     ->expect(['dd', 'dump', 'ray'])
     ->each->not->toBeUsed();
 
-arch('ensure `env` is only used in config files')
+it('ensures `env` is only used in config files')
     ->expect('env')
     ->not->toBeUsed()
     ->ignoring('config');
