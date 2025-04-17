15e4c76b6ebe247d297a378957ec2391c7da3a61
GigaGiorgadze <giga.giorgadze.11@gmail.com>
2025-04-17 14:15:14 +0400

    chore: update arch and delete info

--- tests/ArchTest.php
+++ tests/ArchTest.php
@@ -1,7 +1,7 @@
 <?php
 
 it('will not use debugging functions')
-    ->expect(['dd', 'dump', 'ray'])
+    ->expect(['dd', 'dump', 'ray', 'info'])
     ->each->not->toBeUsed();
 
 it('ensures `env` is only used in config files')
